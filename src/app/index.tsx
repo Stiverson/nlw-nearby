23e28e0c606b1a98e74d1b5cb31a657776a7a65d
tiagrobill <ti@agrobill.com.br>
2024-12-10 16:40:50 -0300

    Adição funcionalidades  aula 1:08

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import {View, Text} from  "react-native"
+import { router, Router } from "expo-router"
 import { Steps } from "@/components/steps"
 import { Button } from "@/components/button"
 import { Welcome } from "@/components/welcome"
@@ -8,7 +9,7 @@ export default function Index(){
         <View style={{flex: 1, padding: 40, gap: 40}}>
            <Welcome />
            <Steps />
-           <Button>
+           <Button onPress={() => router.navigate("/home")}>
             <Button.Title>Começar</Button.Title>
            </Button>
         </View>
